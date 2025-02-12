e5dfd903bfba389e2f6a9bd58fcaaa1df907e8b0
Bill Killpack <trombonebill6912@gmail.com>
2025-02-11 20:10:22 -0700

    Did page routing in react

--- startup-react/src/app.jsx
+++ startup-react/src/app.jsx
@@ -2,38 +2,60 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
-export default function App() {
-  return <div className="body bg-dark text-light">
-    <header className="container-fluid">
-      <h1>Rate My Dryer (And Washer)</h1>
-      <nav className="navbar">
-        <menu className="navbar-nav">
-          <img src="RateMyDryerLogo.png" width="50px" height="50px" />
-          <li className="nav-item">
-            <a className="nav-link active" href="index.html">Home</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="browse.html">Browse</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="review.html">Submit a review</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="find.html">Laundromats near me</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="about.html">About</a>
-          </li>
-        </menu>
-      </nav>
-    </header>
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { About } from './about/about';
+import { Browse } from './browse/browse';
+import { Find } from './find/find';
+import { Review } from './review/review';
 
-    <main>App data will go here</main>
-    
-    <footer>
-      <span>Author: Bill Killpack</span>
-      <br />
-      <a href="https://github.com/Billpack68/startup">GitHub</a>
-    </footer>
-  </div>;
+export default function App() {
+  return (
+    <BrowserRouter>
+      <div className="body bg-dark text-light">
+        <header className="container-fluid">
+          <h1>Rate My Dryer (And Washer)</h1>
+          <nav className="navbar">
+            <menu className="navbar-nav">
+              <img src="RateMyDryerLogo.png" width="50px" height="50px" />
+              <li className="nav-item">
+                <NavLink className="nav-link" to="">Login</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to='browse'>Browse</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to='review'>Review</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to='find'>Laundromats near me</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to='about'>About</NavLink>
+              </li>
+            </menu>
+          </nav>
+        </header>
+
+        <Routes>
+          <Route path='/' element={<Login />} exact />
+          <Route path='/about' element={<About />} />
+          <Route path='/browse' element={<Browse />} />
+          <Route path='/find' element={<Find />} />
+          <Route path='/review' element={<Review />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+                
+        <footer>
+          <span>Author: Bill Killpack</span>
+          <br />
+          <a href="https://github.com/Billpack68/startup">GitHub</a>
+        </footer>
+      </div>;
+    </BrowserRouter>
+  );
+}
+
+function NotFound() {
+  return <main className="container-fluid bg-secondary text-center">404: Return to sender. Address unknown.</main>;
 }
