4e12fc61cee2ae8d3c9e5b01ba811d5d75f8d46c
Bill Killpack <trombonebill6912@gmail.com>
2025-02-27 23:35:22 -0700

    Added search for reviews feature

--- startup-react/src/browse/browse.jsx
+++ startup-react/src/browse/browse.jsx
@@ -5,6 +5,7 @@ export function Browse({user}) {
   const [apartment, setApartment] = React.useState('');
   const [building, setBuilding] = React.useState('');
   const [number, setNumber] = React.useState('');
+  const [matchingReviews, setMatchingReviews] = React.useState([]); // Use state for reviews
   const isFormValid = apartment.trim() !== "" && building.trim() !== "" && number.trim() !== "";
 
   function apartmentChange(e) {
@@ -19,18 +20,37 @@ export function Browse({user}) {
     setNumber(e.target.value);
   }
 
-  function findReviews() {
-    console.log(apartment);
-    console.log(building);
-    console.log(number);
+  class Review {
+    constructor(apartment, building, number, date, user, rating, reviewText) {
+      this.apartment = apartment;
+      this.building = building;
+      this.number = number;
+      this.date = date;
+      this.user = user;
+      this.rating = rating;
+      this.reviewText = reviewText;
+    }
+  }
+
+  function findReviews(event) {
+    event.preventDefault();
+    const reviews = JSON.parse(localStorage.getItem('reviews')) || [];
+    const matchedReviews = reviews.filter(review => 
+      review.apartment === apartment && review.building === building && review.number === number
+    );
+
+    if (matchedReviews.length === 0) {
+      matchedReviews.push(new Review('Heritage Halls', '8', '11', '2025-01-01', 'FakeUserMan', '5', 'Did not actually use this unit but here is a review cause why not'));
+    }
+
+    setMatchingReviews(matchedReviews);
   }
 
   return (
     <main>
       <p>Currently logged in as {user}</p>
       
-      
-      <form>
+      <form onSubmit={findReviews}>
         <div className="form-group">
           <label htmlFor="apartmentComplex">Apartment complex</label>
           <input type="text" onChange={apartmentChange} className="form-control" id="apartmentComplex" placeholder="Heritage Halls" />
@@ -43,7 +63,7 @@ export function Browse({user}) {
           <label htmlFor="unitNumber">Washer/Dryer Number</label>
           <input type="number" onChange={numberChange} className="form-control" id="unitNumber" placeholder="11" />
         </div>
-        <button type="submit" onClick={findReviews} className="btn btn-primary" disabled={!isFormValid}>Search</button>
+        <button type="submit" className="btn btn-primary" disabled={!isFormValid}>Search</button>
       </form>
 
       <table>
@@ -56,18 +76,14 @@ export function Browse({user}) {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>2/3/2025</td>
-            <td>ClothesMan3000</td>
-            <td>1/5</td>
-            <td>Clothes didn't dry at all!</td>
-          </tr>
-          <tr>
-            <td>2/1/2025</td>
-            <td>iWearPantsToo</td>
-            <td>3/5</td>
-            <td>Have to run it for an extra 20 minutes</td>
-          </tr>
+          {matchingReviews.map((review, index) => (
+            <tr key={index}>
+              <td>{review.date}</td>
+              <td>{review.user}</td>
+              <td>{review.rating}/5</td>
+              <td>{review.reviewText}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
       <br />
