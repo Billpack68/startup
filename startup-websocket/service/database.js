ee83517a2609c4aa910aa48a1139b86932089575
Bill Killpack <trombonebill6912@gmail.com>
2025-04-07 18:26:33 -0600

    Created startup-websocket

---
+++ startup-websocket/service/database.js
@@ -0,0 +1,70 @@
+const { MongoClient } = require('mongodb');
+const config = require('./dbConfig.json');
+
+const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+const client = new MongoClient(url);
+const db = client.db('startup');
+const userCollection = db.collection('user');
+const reviewCollection = db.collection('review');
+
+// This will asynchronously test the connection and exit the process if it fails
+(async function testConnection() {
+  try {
+    await db.command({ ping: 1 });
+    console.log(`Connect to database`);
+  } catch (ex) {
+    console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+    process.exit(1);
+  }
+})();
+
+function getUser(email) {
+  console.log("Getting user by email");
+  return userCollection.findOne({ email: email });
+}
+
+function getUserByToken(token) {
+  console.log("Getting user by token");
+  return userCollection.findOne({ token: token });
+}
+
+async function addUser(user) {
+  console.log("Adding a user");
+  await userCollection.insertOne(user);
+}
+
+async function updateUser(user) {
+  console.log("Updating user");
+  await userCollection.updateOne({ email: user.email }, { $set: user });
+}
+
+async function addReview(review) {
+  console.log("Adding a review");
+  return reviewCollection.insertOne(review);
+}
+
+function getReviews() {
+  console.log("Getting reviews");
+  const now = new Date();
+  const tmr = new Date(now.getTime() + (24 * 60 * 60 * 1000)); // Because of UTC being ahead, probably unneccessary in most cases but oh well
+  const past28Days = new Date(now.getTime() - (28 * 24 * 60 * 60 * 1000));
+
+  // console.log("Querying reviews from:", past28Days, "to:", tmr);
+
+  const query = { date: { $gt: past28Days.toISOString(), $lt: tmr.toISOString() } };
+  const options = {
+    sort: { date : -1},
+    limit: 100,
+  };
+  const cursor = reviewCollection.find(query, options);
+  return cursor.toArray();
+}
+
+module.exports = {
+  getUser,
+  getUserByToken,
+  addUser,
+  updateUser,
+  addReview,
+  getReviews,
+};
---
+++ startup-websocket/src/browse/browse.jsx
@@ -0,0 +1,134 @@
+import React from 'react';
+import './browse.css';
+
+export function Browse({user}) {
+  const [apartment, setApartment] = React.useState('');
+  const [building, setBuilding] = React.useState('');
+  const [number, setNumber] = React.useState('');
+  const [rating, setRating] = React.useState('');
+  const [matchingReviews, setMatchingReviews] = React.useState([]); // Use state for reviews
+  const isFormValid = apartment.trim() !== "" && building.trim() !== "";
+
+  function apartmentChange(e) {
+    setApartment(e.target.value);
+  }
+
+  function buildingChange(e) {
+    setBuilding(e.target.value);
+  }
+
+  function numberChange(e) {
+    setNumber(e.target.value);
+  }
+
+  function ratingChange(e) {
+    setRating(e.target.value);
+  }
+
+  class Review {
+    constructor(apartment, building, number, date, user, rating, reviewText) {
+      this.apartment = apartment;
+      this.building = building;
+      this.number = number;
+      this.date = date;
+      this.user = user;
+      this.rating = rating;
+      this.reviewText = reviewText;
+    }
+  }
+
+  async function findReviews(e) {
+    e.preventDefault();
+    let matchedReviews = [];
+    try {
+      const reviewsResponse = await fetch('/api/reviews', {
+        method: 'GET',
+      });
+      if (!reviewsResponse.ok) {
+        throw new Error('Failed to fetch reviews');
+      }
+      const reviewsData = await reviewsResponse.json();
+
+      if (number != "") {
+        if (rating != "") {
+          matchedReviews = reviewsData.filter(review => 
+            review.apartment === apartment && review.building === building && review.number === number && review.rating === rating);
+        } else {
+          matchedReviews = reviewsData.filter(review => 
+            review.apartment === apartment && review.building === building && review.number === number);
+          }
+      } else {
+        if (rating != "") {
+          matchedReviews = reviewsData.filter(review => 
+            review.apartment === apartment && review.building === building && review.rating === rating);
+        } else {
+          matchedReviews = reviewsData.filter(review => 
+            review.apartment === apartment && review.building === building);
+        }
+      }
+    } catch (error) {
+      console.error('Error fetching or filtering reviews:', error);
+    }
+
+    if (matchedReviews.length === 0) {
+      matchedReviews.push(new Review('Heritage Halls', '8', '', '', '', '?', 'No reviews yet!'));
+    }
+
+    setMatchingReviews(matchedReviews);
+  }
+
+  return (
+    <main>
+      <p>Currently logged in as: {user}</p>
+
+      <p>Enter an apartment complex and building number to find recent reviews left by people on washers/dryers in that building!
+        Optionally include a specific washer/dryer number or a specific rating (1-5) to filter results further.
+        Or, click on "leave a review" above to leave your own review!
+      </p>
+      
+      <form onSubmit={findReviews}>
+        <div className="form-group">
+          <label htmlFor="apartmentComplex">Apartment complex</label>
+          <input type="text" onChange={apartmentChange} className="form-control" id="apartmentComplex" placeholder="Heritage Halls" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="buildingNumber">Building Number</label>
+          <input type="number" onChange={buildingChange} className="form-control" id="buildingNumber"/>
+        </div>
+        <div className="form-group">
+          <label htmlFor="unitNumber">Washer/Dryer Number (optional)</label>
+          <input type="number" onChange={numberChange} className="form-control" id="unitNumber"/>
+        </div>
+        <div className="form-group">
+          <label htmlFor="unitNumber">Rating (optional)</label>
+          <input type="number" onChange={ratingChange} className="form-control" id="unitNumber"/>
+        </div>
+        <button type="submit" className="btn btn-primary" disabled={!isFormValid}>Search</button>
+      </form>
+
+      <table>
+        <thead>
+          <tr>
+            <th className="col1">Date</th>
+            <th className="col2">User</th>
+            <th className="col3">Number</th>
+            <th className="col4">Rating</th>
+            <th className="col5">Review</th>
+          </tr>
+        </thead>
+        <tbody>
+          {matchingReviews.map((review, index) => (
+            <tr key={index}>
+              <td>{review.date.slice(5,7)}/{review.date.slice(8,10)}/{review.date.slice(0,4)}</td>
+              <td>{review.user}</td>
+              <td>{review.number}</td>
+              <td>{review.rating}/5</td>
+              <td>{review.reviewText}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <br />
+    </main>
+  );
+}
